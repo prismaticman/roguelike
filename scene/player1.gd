f76173db0378d17de56cd08af3a2336661c3cc83
27794 <2779435956@qq.com>
2023-07-03 11:52:50 +0800

    doc[doc]:v1.0

--- scene/player1.gd
+++ scene/player1.gd
@@ -5,8 +5,6 @@ export var speed = 260    # 设定角色移动速度
 var motion = Vector2()      # 用于保存角色移动的向量
 onready var sprite = $Sprite
 
-
-
 func _ready():
 	pass                   # 当节点第一次加载时执行的函数
 
@@ -20,6 +18,6 @@ func _physics_process(delta):
 	else:
 		sprite.play("walk")
 		$Sprite.flip_h = motion.x<0
-	
+
 	motion *= speed          # 根据角色速度来缩放motion向量
 	motion = move_and_slide(motion)  # 使用move_and_slide()函数来将角色移动
